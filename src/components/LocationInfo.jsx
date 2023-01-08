d1453e5ed59acf19e9e56561cb0369365f52f999
jccod <jcherbens@gmail.com>
2023-01-07 23:41:12 -0500

    Location info box

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import './styles/locationInfo.css'
 
-const LocationInfo = ({location}) => {
+const LocationInfo = ({ location }) => {
 
     return (
-        <article>
-            <h2>{location?.name}</h2>
-            <ul>
-                <li><span>Type: </span>{location?.type}</li>
-                <li><span>Dimension: </span>{location?.dimension}</li>
-                <li><span>Population: </span>{location?.residents.length}</li>
+        <article className='box'>
+            <h2 className='box__location'>{location?.name}</h2>
+            <ul className='box__items-container'>
+                <li className='box__item'>
+                    <span className='box__label'>Type: </span>{location?.type}
+                </li>
+                <li className='box__item'>
+                    <span className='box__label'>Dimension: </span>{location?.dimension}
+                </li>
+                <li className='box__item'>
+                    <span className='box__label'>Population: </span>{location?.residents.length}
+                </li>
             </ul>
         </article>
     );
